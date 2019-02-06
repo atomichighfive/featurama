0cc5cf21819eaa4c4c64505464c79d99660c2b1b
Andreas Syrén <>
2019-02-06 20:13:31 +0100

    Moved sample data

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import dask.dataframe as dd
 from featurama.interactions.numeric import pairwise
 
 #%%
-data = dd.read_csv('./titanic.csv')
+data = dd.read_csv('./featurama/sample_data/titanic.csv')
 data.head()
 
 #%%
